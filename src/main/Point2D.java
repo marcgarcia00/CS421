23505af9b18e06476b39eaa34fbe5ef354679e27
Marc Garcia <mgarcia@catalyte.io>
2018-10-04 20:53:31 -0500

    Added CCW method

--- src/main/Point2D.java
+++ src/main/Point2D.java
@@ -2,4 +2,14 @@ package main;
 
 public class Point2D {
 
+	public int counterClockwise(Point a, Point b, Point c) {
+		int area = (b.getX() - a.getX()) * (c.getY() - a.getY()) - (b.getY() - a.getY()) * (c.getX() - a.getX());
+		if (area < 0)
+			return -1; // clockwise
+		else if (area > 0)
+			return 1; // counter-clock
+		else {
+			return 0;
+		} // same line: colinear
+	}
 }
